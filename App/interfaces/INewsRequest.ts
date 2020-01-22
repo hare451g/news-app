f5335bf7c50b6e8fbd6d24be907acc7fbf17289d
hendra.sadewa <hendra.sadewa@hara.ag>
2020-01-22 16:11:07 +0700

    define news request params interface

---
+++ App/interfaces/INewsRequest.ts
@@ -0,0 +1,10 @@
+interface INewsRequest {
+  country?: string;
+  category?: string;
+  sources?: string;
+  pageSize?: number;
+  page?: number;
+  apiKey: string;
+}
+
+export default INewsRequest;
